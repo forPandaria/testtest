811e9cc0c711f96985b3b7b87826e71f847340b8
forPandaria <312278549@qq.com>
2017-11-24 23:26:36 +0800

    helloworld

---
+++ src/main/java/yui/com/dom.java
@@ -0,0 +1,11 @@
+package yui.com;
+
+/**
+ * Created by yui on 2017/11/24.
+ */
+public class dom {
+    public static void main(String[] args) {
+
+        System.out.print("helloworld");
+    }
+}
